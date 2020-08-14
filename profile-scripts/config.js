632bb9bd4944626d4e5dc49014a1f5613851589d
benedictws <3626837+benedictws@users.noreply.github.com>
2020-08-14 11:16:57 +0100

    Create config.js

---
+++ profile-scripts/config.js
@@ -0,0 +1,19 @@
+
+var respecConfig = {
+  specStatus: "CG-DRAFT",
+  editors: [{
+      name: "Benedict Whittam Smith",
+      company: "Refinitiv",
+      companyURL: "https://www.refinitiv.com",
+  }, {
+      name: "Mark Bird",
+      company: "Data BP",
+      companyURL: "https://www.databp.com/",
+  }],
+  wg: "Rights Automation for Market Data Community Group",
+  wgURI: "https://www.w3.org/community/md-odrl-profile/",
+  wgPublicList: "https://lists.w3.org/Archives/Public/public-md-odrl-profile/",
+  gitHub: "w3c/market-data-odrl-profile",
+  shortName: "md-odrl-profile",
+  //xref: "odrl",
+};
