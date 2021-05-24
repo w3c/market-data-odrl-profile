2c52985b9903152974c9315a51bd88f652a2d0a8
benedictws <3626837+benedictws@users.noreply.github.com>
2021-05-24 13:29:30 +0100

    Update config.js

--- profile-scripts/config.js
+++ profile-scripts/config.js
@@ -12,7 +12,7 @@ var respecConfig = {
   }],
   wg: "Rights Automation for Market Data Community Group",
   wgURI: "https://www.w3.org/community/md-odrl-profile/",
-  wgPublicList: "https://lists.w3.org/Archives/Public/public-md-odrl-profile/",
+  wgPublicList: "https://lists.w3.org/Archives/Public/public-md-odrl-profile",
   gitHub: "w3c/market-data-odrl-profile",
   shortName: "md-odrl-profile",
   //xref: "odrl",
