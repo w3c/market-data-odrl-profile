b9e4ab1031f6efc42104bab892f0ba9a10704e5e
benedictws <3626837+benedictws@users.noreply.github.com>
2021-08-31 19:35:05 +0100

    Update config.js

--- profile-scripts/config.js
+++ profile-scripts/config.js
@@ -19,13 +19,43 @@ var respecConfig = {
       LEI: {
         title: "ISO 17442: Legal Entity Identifiers (LEI)",
         href: "https://www.gleif.org/en/about-lei/iso-17442-the-lei-code-structure/",
-        status: "RSCND",
+        status: "",
         publisher: "ISO",
       },
-      CFI: {
-        title: "ISO 17442: Market Identifier Codes (CFI)",
+      ISO17442: {
+        title: "ISO 17442: Market Identifier Codes (MIC)",
         href: "https://www.iso20022.org/market-identifier-codes",
-        status: "RSCND",
+        status: "",
+        publisher: "ISO",
+      },
+      ATS: {
+        title: "17 CFR 242.300 - Definitions",
+        href: "https://www.govinfo.gov/app/details/CFR-2012-title17-vol3/CFR-2012-title17-vol3-sec242-300",
+        status: "Definition of Alternative Trading System (ATS)",
+        publisher: "SEC",
+      },
+      MiFID: {
+        title: "MIFID II, TITLE I, ARTICLE 4 - DEFINITIONS",
+        href: "https://www.esma.europa.eu/databases-library/interactive-single-rulebook/clone-mifid-ii/article-4-0",
+        status: "Definitions for multilateral trading facilities (MTF), organised trading facilities (OTF), and systematic internalization (SI)",
+        publisher: "ESMA",
+      },
+      EXCHANGE: {
+        title: "15 U.S.C. 78c - Definitions and application",
+        href: "https://www.govinfo.gov/app/details/USCODE-2010-title15/USCODE-2010-title15-chap2B-sec78c",
+        status: "Definition for exchange",
+        publisher: "SEC",
+      },
+      ECN: {
+        title: "17 CFR § 242.600 - NMS security designation and definitions.",
+        href: "https://www.govinfo.gov/app/details/CFR-2008-title17-vol3/CFR-2008-title17-vol3-sec242-600",
+        status: "Definition for electronic communications network",
+        publisher: "SEC",
+      },
+      ISO10962: {
+        title: "ISO 10962 - Classification of financial instruments (CFI) code.",
+        href: "https://www.iso.org/standard/81140.html",
+        status: "",
         publisher: "ISO",
       },
     },
