f463fe6b82dc94b94435a6facaadb35e6001376e
benedictws <3626837+benedictws@users.noreply.github.com>
2021-08-31 15:31:09 +0100

    localBiblio

--- profile-scripts/config.js
+++ profile-scripts/config.js
@@ -1,19 +1,32 @@
-
 var respecConfig = {
-  specStatus: "CG-DRAFT",
-  editors: [{
-      name: "Benedict Whittam Smith",
-      company: "Refinitiv",
-      companyURL: "https://www.refinitiv.com",
-  }, {
-      name: "Mark Bird",
-      company: "Data BP",
-      companyURL: "https://www.databp.com/",
-  }],
-  wg: "Rights Automation for Market Data Community Group",
-  wgURI: "https://www.w3.org/community/md-odrl-profile/",
-  wgPublicList: "https://lists.w3.org/Archives/Public/public-md-odrl-profile",
-  gitHub: "w3c/market-data-odrl-profile",
-  shortName: "md-odrl-profile",
-  //xref: "odrl",
-};
+    specStatus: "CG-DRAFT",
+    editors: [{
+        name: "Benedict Whittam Smith",
+        company: "Refinitiv",
+        companyURL: "https://www.refinitiv.com",
+    }, {
+        name: "Mark Bird",
+        company: "Data BP",
+        companyURL: "https://www.databp.com/",
+    }],
+    wg: "Rights Automation for Market Data Community Group",
+    wgURI: "https://www.w3.org/community/md-odrl-profile/",
+    wgPublicList: "https://lists.w3.org/Archives/Public/public-md-odrl-profile",
+    gitHub: "w3c/market-data-odrl-profile",
+    shortName: "md-odrl-profile",
+    //xref: "odrl",
+    localBiblio: {
+      LEI: {
+        title: "ISO 17442: Legal Entity Identifiers (LEI)",
+        href: "https://www.gleif.org/en/about-lei/iso-17442-the-lei-code-structure/",
+        status: "RSCND",
+        publisher: "ISO",
+      },
+      CFI: {
+        title: "ISO 17442: Market Identifier Codes (CFI)",
+        href: "https://www.iso20022.org/market-identifier-codes",
+        status: "RSCND",
+        publisher: "ISO",
+      },
+    },
+  };
