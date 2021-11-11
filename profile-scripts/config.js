67aa0d8962d5ee19f0cb0ac33fd5f0d992908b34
benedictws <3626837+benedictws@users.noreply.github.com>
2021-11-11 18:07:15 +0000

    Update config.js

--- profile-scripts/config.js
+++ profile-scripts/config.js
@@ -11,8 +11,6 @@ var respecConfig = {
         company: "Data BP",
         companyURL: "https://www.databp.com/",
     }],
-    wg: "Rights Automation for Market Data Community Group",
-    wgURI: "https://www.w3.org/community/md-odrl-profile/",
     wgPublicList: "https://lists.w3.org/Archives/Public/public-md-odrl-profile",
     gitHub: "w3c/market-data-odrl-profile",
     shortName: "md-odrl-profile",
